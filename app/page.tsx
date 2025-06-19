52f43e62fe7827adc90ec1ae2d8cacb92a13c280
jmssnr <maussner.johannes@gmail.com>
2025-06-19 12:06:16 +0200

    makes screen fullscreen

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,21 @@
 
 import Scene from "@/components/scene";
 import SVGEngine from "@/components/svg-engine";
+import { ParentSize } from "@visx/responsive";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen grid place-content-center">
-      <Scene width={1024} height={640}>
-        <SVGEngine />
-      </Scene>
+    <div className="w-screen h-screen">
+      <ParentSize>
+        {({ width, height }) => {
+          if (width === 0 || height === 0) return;
+          return (
+            <Scene width={width} height={height}>
+              <SVGEngine />
+            </Scene>
+          );
+        }}
+      </ParentSize>
     </div>
   );
 }
